3021a7b25515fe0b83fa1a347486c6a04dfe6b01
diego <diegoeliseoiovane@gmail.com>
2024-03-12 21:23:08 -0300

    refactor to create woo order on server - working deploy 12.3 21.21

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require("express")
 const { Server } = require("socket.io")
 const { createServer } = require("http")
@@ -10,26 +11,94 @@ app.use(cors())
 app.use(express.json());
 
 //FOR PROD
-// const io = new Server(httpServer)
-// app.use(express.static('dist'))
+const io = new Server(httpServer)
+app.use(express.static('dist'))
 
 //FOR DEV
-const io = new Server(httpServer, {
-    cors: {
-        origin: "http://localhost:5173"
-    }
-})
+// const io = new Server(httpServer, {
+//     cors: {
+//         origin: "http://localhost:5173"
+//     }
+// })
 
 io.on("connection", socket => {
     console.log("user connected")
 })
 
-app.post("/api/new-order", (req, res) => {
-    
-    const data = req.body
+app.post("/api/new-order", async (req, res) => {
 
-    io.emit("new-order", { success: true, data: data })
-    res.send("ok").status(200)
+    const baseUrl = process.env.STORE_URL
+    const ck = process.env.WOO_CONSUMER_KEY
+    const cs = process.env.WOO_SECRET_KEY
+
+    //CREATE WOO ORDER
+    const url = `${baseUrl}orders`
+    const auth = btoa(`${ck}:${cs}`)
+
+    const data = {
+        payment_method: 'bacs',
+        payment_method_title: 'Direct Bank Transfer',
+        set_paid: true,
+        billing: {
+            first_name: 'John',
+            last_name: 'Doe',
+            address_1: '969 Market',
+            address_2: '',
+            city: 'San Francisco',
+            state: 'CA',
+            postcode: '94103',
+            country: 'US',
+            email: 'john.doe@example.com',
+            phone: '(555) 555-5555',
+        },
+        shipping: {
+            first_name: 'Takeaway',
+            last_name: 'Doe',
+            address_1: '969 Market',
+            address_2: '',
+            city: 'San Francisco',
+            state: 'CA',
+            postcode: '94103',
+            country: 'US',
+        },
+        line_items: [
+            {
+                product_id: req.body.id,
+                quantity: 1,
+            },
+        ],
+        shipping_lines: [
+            {
+                method_id: 'flat_rate',
+                method_title: 'Flat Rate',
+                total: '10.00',
+            },
+        ],
+    };
+
+    try {
+
+        const wooResponse = await fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Basic ${auth}`,
+            },
+            body: JSON.stringify(data),
+        })
+
+        const result = await wooResponse.json()
+
+    } catch (err) {
+
+        res.status(500).send({ ok: false, msg: err })
+
+    }
+
+    const orderData = req.body
+    io.emit("new-order", { success: true, data: orderData })
+
+    res.send({ ok: true }).status(200)
 })
 
 httpServer.listen(8080, () => {
